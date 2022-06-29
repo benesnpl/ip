2b9de05e30185aa34a7b1ac677c5767e6a03c7e5
Benelab <78307945+benesnpl@users.noreply.github.com>
2022-06-29 15:25:21 -0500

    Create publicip.tf

---
+++ publicip.tf
@@ -0,0 +1,9 @@
+resource "azurerm_public_ip" "management" {
+  for_each            = var.vmseries
+  name                = "${each.key}-nic-management-pip"
+  location            = var.resource_location
+  resource_group_name = var.resource_group_name
+  allocation_method   = "Static"
+  depends_on          = [azurerm_resource_group.this]
+  sku                 = "Standard"
+}
