659db62638f4682a329950e1452d5858b4659142
Benelab <78307945+benesnpl@users.noreply.github.com>
2022-06-29 15:28:43 -0500

    Update publicip.tf

--- publicip.tf
+++ publicip.tf
@@ -4,6 +4,5 @@ resource "azurerm_public_ip" "management" {
   location            = var.resource_location
   resource_group_name = var.resource_group_name
   allocation_method   = "Static"
-  depends_on          = [azurerm_resource_group.this]
   sku                 = "Standard"
 }
