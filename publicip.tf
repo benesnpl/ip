4b1f0f760c8b78ed7703fb4b43c756b9182d9dca
Benelab <78307945+benesnpl@users.noreply.github.com>
2022-06-29 15:31:51 -0500

    Update publicip.tf

--- publicip.tf
+++ publicip.tf
@@ -1,3 +1,8 @@
+resource "azurerm_resource_group" "this" {
+  location = var.resource_location
+  name     = var.resource_group_name
+}
+
 resource "azurerm_public_ip" "management" {
   for_each            = var.vmseries
   name                = "${each.key}-nic-management-pip"
